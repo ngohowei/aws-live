744525c2cb650cef33e1eb373acac25255d125e0
ngohowei <158136017+ngohowei@users.noreply.github.com>
2024-04-24 18:14:05 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c7ww8gyeielu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3653"
+customdb = "employee"
+custombucket = "ngohowei-employee"
+customregion = "us-east-1"
+
